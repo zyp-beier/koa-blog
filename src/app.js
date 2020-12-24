cd9e94e39c866fda5d3a99464305c87f185d85bf
zyp贝儿i <1528466340@qq.com>
2020-12-24 19:42:05 +0800

    a

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-const Koa = require('koa')
-const Route = require('@koa/router')
-const serve = require('koa-static')
-const njs = require('koa-nunjucks-2')
-const app = new Koa()
-const route = new Route()
-const path = require('path')
-const config = require('../cofig/cofig')
+const Koa = require('koa');
+const Route = require('@koa/router');
+const serve = require('koa-static');
+const njs = require('koa-nunjucks-2');
+const app = new Koa();
+const route = new Route();
+
+const path = require('path');
+const config = require('../cofig/cofig');
+
+const list = require('./stor/list');
+const userInfo = require('./stor/blog');
+
 
 app.use(njs({
     path: path.join(__dirname, 'view'),
@@ -13,42 +18,44 @@ app.use(njs({
     nunjucksConfig:{
         trimBlock: true
     }
-}))
-
+}));
 
 route.get('/', async ctx => {
-    const time = new Date()
-    await ctx.render('index',{
-        name: 'my blog',
-        time: time
-    })
+    await  ctx.render('login/login')
 })
+
+route.get('/index', async ctx => {
+    await ctx.render('index',{
+        userInfo,
+        year:new Date().getFullYear()
+    })
+});
 
 route.get('/list', async ctx => {
-    const list = [
-        {
-            id: 1,
-            title: '第一个日志'
-        },
-        {
-            id: 1,
-            title: '第一个日志'
-        },
-        {
-            id: 1,
-            title: '第一个日志'
-        }
-    ]
     await ctx.render('list',{
-        list:list
+        list
     })
-})
+});
 
-app.use(route.routes())
+route.get('/item/:id', async ctx => {
+    const {id = ''} = ctx.params
+    const item = list.find(item => Number(id) === Number(item.id))
+    console.log(item);
+    if(!item){
+        ctx.status = 404;
+        return
+    }
+    await ctx.render('item',{
+        id,
+        item
+    })
+});
+
+app.use(route.routes());
 app.use(route.allowedMethods());
-app.use(serve(path.join(__dirname,'../public')))
+app.use(serve(path.join(__dirname,'../public')));
 
 
 app.listen(config.port, function() {
-    console.log('http://127.0.0.1:3000');
-})  
+    console.log('http://127.0.0.1:8081');
+});
---
+++ src/stor/blog.js
@@ -0,0 +1,51 @@
+module.exports = {
+  userName: '赵银萍',
+  city: '杭州',
+  job: 'fontEnd',
+  avatar: 'http://pic1.win4000.com/pic/f/24/7ede4c9413.jpg',
+  articleNumber:5,
+  classification: 7,
+  tag: 6,
+  indexData:[
+    {
+      id: 1,
+      title: '这是第一个博客',
+      content: '常量表达式：用于作为输入量Constant 基本常量最基本单个数值常量，快捷键数字键1按住->鼠标左键。Constant2Vector 二维向量常量快捷键数字键2按住->鼠标左键。可指定两个数值，与颜色值(R,G)对应。可用于设置材质颜色等（下同）。onstant3Vector 三维向量常量捷键数字键3按住->鼠标左键。可指定三个值，与颜色值(R,G,B)对Constant4Vector 四维向量常量快捷键数字键4按住->鼠标左键。可指定四个值，与颜色值(R,G,B,A)对应。',
+      tag: 'javascript',
+      createTime: '2020-12-23 15:30:45',
+      pageViews: 158
+    },
+    {
+      id: 2,
+      title: '这是第2个博客',
+      content: '常量表达式：用于作为输入量Constant 基本常量最基本单个数值常量，快捷键数字键1按住->鼠标左键。Constant2Vector 二维向量常量快捷键数字键2按住->鼠标左键。可指定两个数值，与颜色值(R,G)对应。可用于设置材质颜色等（下同）。onstant3Vector 三维向量常量捷键数字键3按住->鼠标左键。可指定三个值，与颜色值(R,G,B)对Constant4Vector 四维向量常量快捷键数字键4按住->鼠标左键。可指定四个值，与颜色值(R,G,B,A)对应。',
+      tag: 'javascript',
+      createTime: '2020-12-23 15:30:45',
+      pageViews: 158
+    },
+    {
+      id: 3,
+      title: '这是第3个博客',
+      content: '常量表达式：用于作为输入量Constant 基本常量最基本单个数值常量，快捷键数字键1按住->鼠标左键。Constant2Vector 二维向量常量快捷键数字键2按住->鼠标左键。可指定两个数值，与颜色值(R,G)对应。可用于设置材质颜色等（下同）。onstant3Vector 三维向量常量捷键数字键3按住->鼠标左键。可指定三个值，与颜色值(R,G,B)对Constant4Vector 四维向量常量快捷键数字键4按住->鼠标左键。可指定四个值，与颜色值(R,G,B,A)对应。',
+      tag: 'javascript',
+      createTime: '2020-12-23 15:30:45',
+      pageViews: 158
+    },
+    {
+      id: 4,
+      title: '这是第4个博客',
+      content: '常量表达式：用于作为输入量Constant 基本常量最基本单个数值常量，快捷键数字键1按住->鼠标左键。Constant2Vector 二维向量常量快捷键数字键2按住->鼠标左键。可指定两个数值，与颜色值(R,G)对应。可用于设置材质颜色等（下同）。onstant3Vector 三维向量常量捷键数字键3按住->鼠标左键。可指定三个值，与颜色值(R,G,B)对Constant4Vector 四维向量常量快捷键数字键4按住->鼠标左键。可指定四个值，与颜色值(R,G,B,A)对应。',
+      tag: 'javascript',
+      createTime: '2020-12-23 15:30:45',
+      pageViews: 158
+    },
+    {
+      id: 5,
+      title: '这是第5个博客',
+      content: '常量表达式：用于作为输入量Constant 基本常量最基本单个数值常量，快捷键数字键1按住->鼠标左键。Constant2Vector 二维向量常量快捷键数字键2按住->鼠标左键。可指定两个数值，与颜色值(R,G)对应。可用于设置材质颜色等（下同）。onstant3Vector 三维向量常量捷键数字键3按住->鼠标左键。可指定三个值，与颜色值(R,G,B)对Constant4Vector 四维向量常量快捷键数字键4按住->鼠标左键。可指定四个值，与颜色值(R,G,B,A)对应。',
+      tag: 'javascript',
+      createTime: '2020-12-23:15:30:45',
+      pageViews: 158
+    },
+  ]
+}
